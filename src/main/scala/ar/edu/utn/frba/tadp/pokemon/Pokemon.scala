cb16543f693a45276413f1b4f4e519d298aa02d5
juanFdS <juan9794@gmail.com>
2023-06-24 12:50:38 -0300

    Codigo clase de objetos-funcional

--- src/main/scala/ar/edu/utn/frba/tadp/pokemon/Pokemon.scala
+++ src/main/scala/ar/edu/utn/frba/tadp/pokemon/Pokemon.scala
@@ -1,10 +1,163 @@
 package ar.edu.utn.frba.tadp.pokemon
 
-object Pokemon {
+import scala.::
+import scala.Option.option2Iterable
+import scala.collection.IterableOnce.iterableOnceExtensionMethods
+import scala.util.Try
+
+// package object definido porque estamos en scala 2
+object GimnasioPokemon {
+
+  // descansar como metodo que produce efecto
+  //  class Pokemon(var energia: Int, var energiaMaxima: Int) {
+  //    def descansar(): Unit = {
+  //      energia = energiaMaxima
+  //    }
+  //  }
+  // descansar como metodo de consulta
+  case class Pokemon(
+                      energiaMaxima: Int,
+                      experiencia: Int,
+                      especie: Especie,
+                      estado: Estado= Bien
+                    )(energiaInicial: Int,
+                      velocidadInicial: Int = 1,
+                      fuerzaInicial: Int = 1) {
+
+    val velocidad =
+      velocidadInicial.max(1).min(100)
+    val energia = this.energiaInicial.max(0)
+    val fuerza =
+      fuerzaInicial.max(1).min(100)
+    def descansar(): Pokemon = cambiarEnergia(energiaMaxima)
+    def nadar(minutos: Int): Try[Pokemon] = Try {
+      estado match {
+        case Bien if pierdeContra(Agua) => cambiarEstadoA(KO)
+        case Bien => {
+          val pokemonDespuesDeNadar = copy(
+            experiencia = experiencia + 200 * minutos)(
+            energiaInicial = energia - minutos,
+            velocidadInicial = velocidad,
+            fuerzaInicial = fuerza
+          )
+          if (tieneTipo(Agua)) {
+            pokemonDespuesDeNadar.copy()(
+              energiaInicial = pokemonDespuesDeNadar.energia,
+              velocidadInicial = pokemonDespuesDeNadar.velocidad + minutos / 60,
+              fuerzaInicial = fuerza
+            )
+          } else pokemonDespuesDeNadar
+      }
+    }
+    }
+    def levantarPesas(kilos: Int): Try[Pokemon] = {
+      Try {
+        this match {
+          case Fantasma() =>
+            throw new RuntimeException("Un fantasma no puede levantar pesas")
+          case pokemon if kilos > 10 * pokemon.fuerza =>
+            pokemon.cambiarEnergia(energia - 10)
+          case Pelea() =>
+            this.cambiarExperiencia(experiencia + kilos * 2)
+          case pokemon =>
+            pokemon.cambiarExperiencia(experiencia + kilos)
+        }
+      }
+    }
+
+    def tieneTipo(tipo: Tipo) = especie.tieneTipo(tipo)
+
+    def cambiarEstadoA(nuevoEstado: Estado) =
+      copy(estado = nuevoEstado)(
+        energiaInicial = energiaInicial, velocidadInicial = velocidadInicial, fuerzaInicial = fuerzaInicial
+      )
+
+    def pierdeContra(unTipo: Tipo) = especie.pierdeContra(unTipo)
+
+    def cambiarExperiencia(nuevaExperiencia: Int): Pokemon = {
+      copy(experiencia = nuevaExperiencia)(
+        energiaInicial = energiaInicial,
+        velocidadInicial = velocidadInicial,
+        fuerzaInicial = fuerza
+      )
+    }
+
+    def cambiarEnergia(nuevaEnergia: Int): Pokemon = {
+      copy()(
+        energiaInicial = nuevaEnergia,
+        velocidadInicial = velocidadInicial,
+        fuerzaInicial = fuerza
+      )
+    }
+
+    // que no tenga el metodo -> que no compile
+    // que me devuelva al pokemon como esta -> no cumple el requerimiento
+    // que tire una excepcion
+    // que devuelva un valor de error (un Try)
+
+  }
+
+  trait Estado
+  case object KO extends Estado
+  case object Bien extends Estado
+  object Especie {
+    def unapply(pokemon: Pokemon) = Some(pokemon.especie)
+  }
+  case class Especie(tipoPrimario: Tipo,
+                     tipoSecundario: Option[Tipo] = None) {
+
+    val tipos = tipoPrimario :: tipoSecundario.toList
+    def tieneTipo(unTipo: Tipo) =
+      tipoPrimario == unTipo || tipoSecundario.contains(unTipo)
+
+    def pierdeContra(unTipo: Tipo): Boolean =
+      tipos.exists(tipo => tipo.pierdeContra(unTipo))
+
+  }
+  class Tipo(debilidades: => List[Tipo] = Nil) {
+    def pierdeContra(otroTipo: Tipo): Boolean =
+      debilidades.contains(otroTipo)
+    def unapply(pokemon: Pokemon) = pokemon tieneTipo this
+      // Otra opcion:
+      //      Option.when(pokemon.tieneTipo(this))(pokemon)
+
+
+    //    def unapply(pokemon: Pokemon) =
+//      if (pokemon.tieneTipo(this)) Option(pokemon) else None
+  }
+  case object Agua extends Tipo()
+  case object Fuego extends Tipo(List(Agua))
+  case object Tierra extends Tipo(List(Agua))
+  case object Roca extends Tipo(List(Agua))
+  case object Electricidad extends Tipo()
+  case object Pelea extends Tipo()
+
+  lazy val Fantasma: Tipo = new Tipo(List(Fantasma))
+  // descansar como funcion
+  //  def descansar(pokemon: Pokemon): Pokemon =
+  //    new Pokemon(energia = energiaMaxima, energiaMaxima = energiaMaxima)
+
+
   def experienciaNecesariaParaNivel(nivel: Int, resistenciaEvolutiva: Int): Int = {
     nivel match {
       case 1 => 0
       case n => resistenciaEvolutiva + experienciaNecesariaParaNivel(n - 1, resistenciaEvolutiva) * 2
     }
   }
+
+  class Actividad(accion: Pokemon => Try[Pokemon]) {
+    def hacerse(pokemon: Pokemon): Try[Pokemon] =
+      pokemon.estado match {
+        case KO =>
+          Try {
+            throw new RuntimeException("Un pokemon no puede hacer una actividad si esta KO")
+          }
+        case Bien => accion(pokemon)
+      }
+  }
+
+  val descansar: Actividad = new Actividad(Try { _.descansar() })
+  def nadar(minutos: Int): Actividad = new Actividad(_.nadar(minutos))
+
+  def levantarPesas(kilos: Int): Actividad = new Actividad(_.levantarPesas(kilos))
 }
--- src/test/scala/ar/edu.utn/frba/tadp/PokemonSuite.scala
+++ src/test/scala/ar/edu.utn/frba/tadp/PokemonSuite.scala
@@ -1,10 +1,14 @@
 package ar.edu.utn.frba.tadp.pokemon
 
-import ar.edu.utn.frba.tadp.pokemon.Pokemon.experienciaNecesariaParaNivel
+import ar.edu.utn.frba.tadp.pokemon.GimnasioPokemon._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-
+import scala.util.Success
 class PokemonSpec extends AnyFlatSpec {
+  val especieCharmander = Especie(tipoPrimario = Fuego)
+  val especiePoliwhirl = Especie(tipoPrimario = Agua)
+  val especiePikachu = Especie(tipoPrimario = Electricidad)
+  val especieMankey = Especie(tipoPrimario = Pelea)
+
   "la experiencia necesaria estar en nivel 1" should "ser 0" in {
     val resistenciaEvolutiva = 1
 
@@ -15,5 +19,104 @@ class PokemonSpec extends AnyFlatSpec {
     val resistenciaEvolutiva = 350
 
     assert(experienciaNecesariaParaNivel(3, resistenciaEvolutiva) === 1050)
+  }
+
+  "descansar" should "llevar la energia de un pokemon a su energia maxima" in {
+    val charmander = Pokemon(
+      energiaMaxima = 100,
+      especie = especieCharmander,
+      experiencia = 0,
+    )(energiaInicial = 10)
+
+    assert(charmander.descansar().energia === 100)
+  }
+
+  "si un pokemon que no es de agua nada" should "disminuir en 1 punto la energia y aumentar en 200 la experiencia por cada minuto nadado" in {
+    val pikachu = Pokemon(
+      energiaMaxima = 100,
+      especie = especiePikachu,
+      experiencia = 0
+    )(energiaInicial = 10)
+    val pikachuEntrenado = pikachu.nadar(2)
+
+    assert(pikachuEntrenado.energia === 8)
+    assert(pikachuEntrenado.experiencia === 400)
+  }
+
+  //TODO: si un pokemon nada mas de lo que le da la energia, deberia aumentar su experiencia tanto como realmente pudo nadar
+
+  "la energia de un pokemon" should "no ser negativa" in {
+    val pikachu = Pokemon(
+      energiaMaxima = 100,
+      especie = especiePikachu,
+      experiencia = 0
+    )(energiaInicial = -5)
+
+    assert(pikachu.energia === 0)
+  }
+
+
+
+  "si un pokemon de tipo agua nada" should "gana 1 punto de velocidad ademas de disminuir su energia y aumentar su experiencia como los demas" in {
+    val poliwhirl = Pokemon(
+      energiaMaxima = 300,
+      especie = especiePoliwhirl,
+      experiencia = 0
+    )(energiaInicial = 200,
+      velocidadInicial = 1,
+      fuerzaInicial = 1)
+
+    val poliwhirlEntrenado = poliwhirl.nadar(120)
+
+    assert(poliwhirlEntrenado.energia === 80)
+    assert(poliwhirlEntrenado.experiencia === 24000)
+    assert(poliwhirlEntrenado.velocidad === 3)
+  }
+
+  "si un pokemon levanta pesas" should "gana 1 punto de experiencia por cada kilo levantado" in {
+    val poliwhirl = Pokemon(
+      energiaMaxima = 300,
+      especie = especiePoliwhirl,
+      experiencia = 0
+    )(energiaInicial = 200,
+      velocidadInicial = 1,
+      fuerzaInicial = 1)
+
+    val Success(poliwhirlEntrenado) = poliwhirl.levantarPesas(5)
+
+    assert(poliwhirlEntrenado.experiencia === 5)
+    assert(poliwhirlEntrenado.energia === poliwhirl.energia)
+
+  }
+
+  "si un pokemon levanta mas de 10 kilos por cada punto de fuerza" should "no gana nada de experiencia y pierde 10 de energia" in {
+    val poliwhirl = Pokemon(
+      energiaMaxima = 300,
+      especie = especiePoliwhirl,
+      experiencia = 0
+    )(energiaInicial = 200,
+      velocidadInicial = 1,
+      fuerzaInicial = 1)
+
+    val Success(poliwhirlEntrenado) = poliwhirl.levantarPesas(20)
+
+    assert(poliwhirlEntrenado.experiencia === poliwhirl.experiencia)
+    assert(poliwhirlEntrenado.energia === 190)
+  }
+
+  "los pokemones de tipo pelea al levantar pesas" should "ganan el doble de puntos de experiencia que el resto" in {
+    val mankey = Pokemon(
+      energiaMaxima = 300,
+      especie = especieMankey,
+      experiencia = 0
+    )(energiaInicial = 200)
+    val poliwhirl = mankey.copy(especie = especiePoliwhirl)(energiaInicial = 200,
+      velocidadInicial = mankey.velocidad,
+      fuerzaInicial = mankey.fuerza)
+
+    val Success(mankeyEntrenado) = mankey.levantarPesas(5)
+    val Success(poliwhirlEntrenado) = poliwhirl.levantarPesas(5)
+
+    assert(mankeyEntrenado.experiencia === poliwhirlEntrenado.experiencia * 2)
   }
 }
